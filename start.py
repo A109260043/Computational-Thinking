785a727658914b2070ef48bb9c07fdeb992db597
A109260043 <71419468+A109260043@users.noreply.github.com>
2020-12-17 12:20:26 +0800

    start.py
    a109260043 week14

---
+++ start.py
@@ -0,0 +1,15 @@
+import turtle
+shelly=turtle.Turtle()
+shelly.shape("turtle")
+shelly.position()
+shelly.color("purple","purple")
+shelly.pencolor("yellow")
+shelly.turtlesize(10,10,2)
+shelly.resizemode("255")
+shelly.turtlesize(5,5,3)
+import turtle
+for i in range(5):
+   shelly.forward(100)
+   shelly.right(144)
+pen.color(255,215,0)
+pen.pensize(5)
